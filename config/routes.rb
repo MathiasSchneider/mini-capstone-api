74e57f4e234009a6a7439f048b6e34ad6bbd426b
Mathias <mathias.richard.schneider@gmail.com>
2021-05-10 13:15:49 -0500

    updates routes and actions to match convention

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-get "/all_products", controller: "products", action: "all_product_method"
-get "/keith_product", controller: "products", action: "keith_product_method"
-get "/ross_product", controller: "products", action: "ross_product_method"
-get "/rizzo_product", controller: "products", action: "rizzo_product_method"
-get "/zobrist_product", controller: "products", action: "zobrist_product_method"
+get "/products", controller: "products", action: "index"
+
+# get "/keith_product", controller: "products", action: "keith_product_method"
+# get "/ross_product", controller: "products", action: "ross_product_method"
+# get "/rizzo_product", controller: "products", action: "rizzo_product_method"
+# get "/zobrist_product", controller: "products", action: "zobrist_product_method"
+
+get "/products/:id", controller: "products", action: "show"
 
 #query parameter
 get "/query_param" => "products#single_param"
