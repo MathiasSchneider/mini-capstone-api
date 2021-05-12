51b534237a7e5d64ab394df64b6fdd0bd8942c41
Mathias <mathias.richard.schneider@gmail.com>
2021-05-12 16:02:03 -0500

    changed data type of description and price. added inventory. enabled seed dump.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
 get "/products", controller: "products", action: "index"
-
-post "products", controller: "products", action: "create"
-
+post "/products", controller: "products", action: "create"
 get "/products/:id", controller: "products", action: "show"
-
-patch "products/:id", controller: "products", action: "update"
-
-delete "products/:id", controller: "products", action: "destroy"
+patch "/products/:id", controller: "products", action: "update"
+delete "/products/:id", controller: "products", action: "destroy"
 
 # #query parameter
 # get "/query_param" => "products#single_param"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-product = Product.create({name: "Keith Krunch", price: 5, image_url: "https://images.squarespace-cdn.com/content/v1/5b5876eac258b425696cecae/1532534778231-I94YFTMNWZ6KTYADAR57/ke17ZwdGBToddI8pDm48kFR6y1dVk8DTTEwB9FP89297gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QHyNOqBUUEtDDsRWrJLTmnm4Ek6c6YJYE3gl9iwgPJP8u2B2Kpk93Y7GXgzDZSllF-K66FO__qH4lNpj5GYDQ/Keith+Krunch?format=300w", description: "Frosted corn flakes"})
-product = Product.create({name: "Rizzos", price: 4, image_url: "https://www.chicagotribune.com/resizer/rqOKvX6is_pBLmHFkwWfBb8zZIU=/800x1000/top/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/2Q6YIXHWRVDHBA3IM7G3BQQN4Q.jpg", description: "Honey nut toasted oats"})
-product = Product.create({name: "Grandpa Rossy Crunch", price: 5, image_url: "https://i5.walmartimages.com/asr/f81ca56a-0399-4e2c-99fb-777f354ef537_1.e6e5ca4d906be1d89578e9123e8f34fb.jpeg?odnWidth=612&odnHeight=612&odnBg=ffffff", description: "Frosted corn flakes"})
-product = Product.create({name: "Zorilla Crunch", price: 6, image_url: "https://d2d8wwwkmhfcva.cloudfront.net/400x/d2lnr5mha7bycj.cloudfront.net/product-image/file/large_a6e53893-7540-4b32-9d8f-221809f7bc7c.jpg", description: "Toasted oats with marshmallows"})
+
+
+# product = Product.create({name: "Keith Krunch", price: 5, image_url: "https://images.squarespace-cdn.com/content/v1/5b5876eac258b425696cecae/1532534778231-I94YFTMNWZ6KTYADAR57/ke17ZwdGBToddI8pDm48kFR6y1dVk8DTTEwB9FP89297gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QHyNOqBUUEtDDsRWrJLTmnm4Ek6c6YJYE3gl9iwgPJP8u2B2Kpk93Y7GXgzDZSllF-K66FO__qH4lNpj5GYDQ/Keith+Krunch?format=300w", description: "Frosted corn flakes"})
+# product = Product.create({name: "Rizzos", price: 4, image_url: "https://www.chicagotribune.com/resizer/rqOKvX6is_pBLmHFkwWfBb8zZIU=/800x1000/top/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/2Q6YIXHWRVDHBA3IM7G3BQQN4Q.jpg", description: "Honey nut toasted oats"})
+# product = Product.create({name: "Grandpa Rossy Crunch", price: 5, image_url: "https://i5.walmartimages.com/asr/f81ca56a-0399-4e2c-99fb-777f354ef537_1.e6e5ca4d906be1d89578e9123e8f34fb.jpeg?odnWidth=612&odnHeight=612&odnBg=ffffff", description: "Frosted corn flakes"})
+# product = Product.create({name: "Zorilla Crunch", price: 6, image_url: "https://d2d8wwwkmhfcva.cloudfront.net/400x/d2lnr5mha7bycj.cloudfront.net/product-image/file/large_a6e53893-7540-4b32-9d8f-221809f7bc7c.jpg", description: "Toasted oats with marshmallows"})
