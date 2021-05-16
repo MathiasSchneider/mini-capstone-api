91ef1e778cfa66e55f9933b2ce4644ecff2bbd3b
Mathias <mathias.richard.schneider@gmail.com>
2021-05-16 15:06:52 -0500

    adds a sort by price and search function to the index action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,45 +1,62 @@
 class ProductsController < ApplicationController
   def index
-    render json: Product.all
+    product = Product.where("name iLIKE ?", "%#{params[:search]}%")
+    render json: product
+  end
+
+  def index
+    products = Product.all
+
+    if params[:search]
+      products = products.where("name iLIKE ?", "%#{params[:search]}%")
+    end
+
+    if params[:sort] == "price"
+      products = products.order(price: :asc)
+    else
+      products = products.order(id: :asc)
+    end
+
+    render json: products
   end
   
-def create
-  product = Product.new(
-    name: params[:name],
-    price: params[:price],
-    image_url: params[:image_url],
-    description: params[:description]
-  )
-  if product.save = true
-    render json: product
-  else
-    render json: product.errors.full_messages
+  def create
+    product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description]
+    )
+    if product.save == true
+      render json: product
+    else
+      render json: product.errors.full_messages
+    end
   end
-end
 
-def show
-  product = Product.find(params[:id])
-  render json: product
-end
-
-def update
-  product = Product.find(params[:id])
-  product.name = params[:name] || product.name
-  product.price = params[:price] || product.price
-  product.image_url = params[:image_url] || product.image_url
-  product.description = params[:description] || product.description
-  if product.save = true
+  def show
+    product = Product.find(params[:id])
     render json: product
-  else
-    render json: product.errors.full_messages
   end
-end
 
-def destroy
-  product = Product.find(params[:id])
-  product.destroy
-  render json: {message: "#{product.name} was successfully destroyed."}
-end
+  def update
+    product = Product.find(params[:id])
+    product.name = params[:name] || product.name
+    product.price = params[:price] || product.price
+    product.image_url = params[:image_url] || product.image_url
+    product.description = params[:description] || product.description
+    if product.save == true
+      render json: product
+    else
+      render json: product.errors.full_messages
+    end
+  end
+
+  def destroy
+    product = Product.find(params[:id])
+    product.destroy
+    render json: {message: "#{product.name} was successfully destroyed."}
+  end
 
 #   #query and segment both use this:
 #   def single_param
